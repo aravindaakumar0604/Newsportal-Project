ab2b04b2ca6065c5822f65afb744a36e40b5146f
aravindaakumar0604 <116151266+aravindaakumar0604@users.noreply.github.com>
2022-10-27 12:03:02 +0530

    Delete project newsportal directory

--- project newsportal/newsportal/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once "database.php";
-session_start();
-unset($_SESSION['aid']);
-unset($_SESSION['ausername']);
-unset($_SESSION['apassword']);
-session_destroy();
-header("Location: index.php");
-?>
--- project newsportal/newsportal/user_logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once "database.php";
-session_start();
-unset($_SESSION['id']);
-unset($_SESSION['username']);
-unset($_SESSION['password']);
-session_destroy();
-header("Location: index.php");
-?>
